e05476b4a11638ca21569c9b6da4697b5829da4c
Lienren <lier@axon.com.cn>
2018-01-09 11:33:54 +0800

    Update

--- index.js
+++ index.js
@@ -2,12 +2,122 @@
  * @Author: Lienren 
  * @Date: 2018-01-08 17:36:54 
  * @Last Modified by: Lienren
- * @Last Modified time: 2018-01-09 10:03:08
+ * @Last Modified time: 2018-01-09 11:32:31
  */
 'use strict';
-let MysqlHelper = require('./mysql-helper');
+const mysql = require('mysql');
 
-module.exports = function({
+class MysqlHelper {
+  constructor({
+    host = '',
+    port = 3306,
+    user = '',
+    password = '',
+    database = '',
+    charset = 'UTF8_GENERAL_CI',
+    dateStrings = true
+  }) {
+    this.default_mysql_connection_config = {
+      ...{
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        database: database,
+        charset: charset,
+        dateStrings: dateStrings
+      }
+    };
+  }
+  open() {
+    this.connection = mysql.createConnection(this.default_mysql_connection_config);
+    this.connection.connect();
+    return this;
+  }
+  close() {
+    if (this.connection) {
+      this.connection.end();
+    }
+    return this;
+  }
+  execute({ sql, values, callback = result => {} }) {
+    if (values) {
+      sql = mysql.format(sql, values);
+    }
+
+    this.connection.query(sql, (err, rows, fields) => {
+      callback({ err: err, rows: rows, fields: fields });
+    });
+    return this;
+  }
+  query({ sql, values }) {
+    return new Promise((resolve, reject) => {
+      this.open()
+        .execute({
+          sql: sql,
+          values: values,
+          callback: result => {
+            if (result.err) {
+              reject(result.err);
+            } else {
+              resolve(result.rows);
+            }
+          }
+        })
+        .close();
+    });
+  }
+  create({ sql, values }) {
+    let get_auto_id = 'SELECT LAST_INSERT_ID() as id;';
+    return new Promise((resolve, reject) => {
+      this.open()
+        .execute({
+          sql: sql,
+          values: values,
+          callback: result => {
+            if (result.err) {
+              reject(result.err);
+            }
+          }
+        })
+        .execute({
+          sql: get_auto_id,
+          callback: result => {
+            if (result.err) {
+              reject(result.err);
+            } else {
+              resolve(result.rows);
+            }
+          }
+        })
+        .close();
+    });
+  }
+  update({ sql, values }) {
+    return new Promise((resolve, reject) => {
+      this.open()
+        .execute({
+          sql: sql,
+          values: values,
+          callback: result => {
+            if (result.err) {
+              reject(result.err);
+            } else {
+              resolve({ rows: result.rows.affectedRows });
+            }
+          }
+        })
+        .close();
+    });
+  }
+  batch(executes, { completeHanding = result => {}, exceptionHandling = err => {} }) {
+    return Promise.all(executes)
+      .then(completeHanding)
+      .catch(exceptionHandling);
+  }
+}
+
+module.exports = ({
   host = '',
   port = 3306,
   user = '',
@@ -15,8 +125,8 @@ module.exports = function({
   database = '',
   charset = 'UTF8_GENERAL_CI',
   dateStrings = true
-}) {
-  let helper = new MysqlHelper({
+}) => {
+  return new MysqlHelper({
     host: host,
     port: port,
     user: user,
@@ -25,5 +135,4 @@ module.exports = function({
     charset: charset,
     dateStrings: dateStrings
   });
-  return helper;
 };
